698d26bd3e0b9b094386f979416b27b88590e1f5
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2020-12-10 15:57:17 +0530

    database

---
+++ imgupload.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2020 at 10:46 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `imgupload`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profileimg`
+--
+
+CREATE TABLE `profileimg` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `profileimg`
+--
+
+INSERT INTO `profileimg` (`id`, `userid`, `status`) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 6, 1),
+(6, 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first` varchar(256) NOT NULL,
+  `last` varchar(256) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first`, `last`, `username`, `password`) VALUES
+(1, 'Jhon', 'Doe', 'Admin', '123'),
+(2, 'Daniel', 'Nilesen', 'Nilesen', '123'),
+(3, 'Atharva', 'Joshi', 'atharva', 'test123'),
+(4, 'Vanita ', 'Joshi', 'vanita', '1234'),
+(6, 'Sandip', 'Joshi', 'sandip', '12345'),
+(8, 'sankalp ', 'singh', 'SANKALPP', '1357');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `profileimg`
+--
+ALTER TABLE `profileimg`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `profileimg`
+--
+ALTER TABLE `profileimg`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
